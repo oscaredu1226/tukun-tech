5511ed25140373dea15ec3dfd000a9af6cc57094
Oscar <oscaredu01@hotmail.com>
2024-11-20 03:52:13 -0500

    asddas

--- src/app/tukun-tech/services/patients/util.service.ts
+++ src/app/tukun-tech/services/patients/util.service.ts
@@ -20,26 +20,26 @@ export class UtilService {
 
 
   getBloodType(): Observable<BloodType[]>{
-    return this.http.get<BloodType[]>(this.baseUrl+'/listBlood')
+    return this.http.get<BloodType[]>('https://tukun-tech-platform.onrender.com/api/v1/util/listBlood')
   }
 
   getNationality(): Observable<Nationality[]>{
-    return this.http.get<Nationality[]>(this.baseUrl+'/listNationality')
+    return this.http.get<Nationality[]>('https://tukun-tech-platform.onrender.com/api/v1/util/listNationality')
   }
 
   getGender(): Observable<Gender[]>{
-    return this.http.get<Gender[]>(this.baseUrl+'/listGender')
+    return this.http.get<Gender[]>('https://tukun-tech-platform.onrender.com/api/v1/util/listGender')
   }
 
   getSpecialization(): Observable<Specialization[]>{
-   return this.http.get<Specialization[]>(this.baseUrl+'/listSpecialization')
+   return this.http.get<Specialization[]>('https://tukun-tech-platform.onrender.com/api/v1/util/listSpecialization')
   }
 
   getAllergy(): Observable<Allergy[]>{
-    return this.http.get<Allergy[]>(this.baseUrl+'/listAllergy')
+    return this.http.get<Allergy[]>('https://tukun-tech-platform.onrender.com/api/v1/util/listAllergy')
   }
   getMedicalInsurance(): Observable<Medicalinsurance[]>{
-    return this.http.get<Medicalinsurance[]>(this.baseUrl+'/listMedicalInsurance')
+    return this.http.get<Medicalinsurance[]>('https://tukun-tech-platform.onrender.com/api/v1/util/listMedicalInsurance')
   }
 
 
